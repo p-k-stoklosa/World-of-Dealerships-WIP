3e3ac76ab2675c024959f9562d830933d8dca9a2
Verbal Centaur <tretti7070@wp.pl>
2021-05-31 11:25:03 +0200

    Updates

---
+++ precache-manifest.d86948570684e41d0a79d4f029bc978c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "596ad202a116de54fce9",
+    "url": "/World-of-Dealerships-WIP/static/css/main.15b86f30.chunk.css"
+  },
+  {
+    "revision": "596ad202a116de54fce9",
+    "url": "/World-of-Dealerships-WIP/static/js/main.3c8721c8.chunk.js"
+  },
+  {
+    "revision": "c8199b3999100c682ed2",
+    "url": "/World-of-Dealerships-WIP/static/js/runtime~main.621c1b6b.js"
+  },
+  {
+    "revision": "75d11719f2be82541e4f",
+    "url": "/World-of-Dealerships-WIP/static/js/2.6edc736b.chunk.js"
+  },
+  {
+    "revision": "6f850a6f226b00f8ea62fe766901046a",
+    "url": "/World-of-Dealerships-WIP/index.html"
+  }
+];
